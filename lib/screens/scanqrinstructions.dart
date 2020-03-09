c0ef730b73f986679bdd38fe18815c86e4b169c3
Fernando García Álvarez <fercarcedo@gmail.com>
2020-03-09 12:56:15 +0100

    Add QR scanning

--- lib/screens/scanqrinstructions.dart
+++ lib/screens/scanqrinstructions.dart
@@ -1,6 +1,9 @@
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:quadrant/i18n/app_messages.dart';
+import 'package:flutter/services.dart';
 
 class ScanQrInstructions extends StatelessWidget {
 
@@ -39,6 +42,7 @@ class ScanQrInstructions extends StatelessWidget {
                   child: SizedBox(height: 50, child: RaisedButton(
                     child: Text(AppMessages.of(context).scanBtn), 
                     onPressed: (){
+                      scan();
                     }, 
                     color: Theme.of(context).accentColor
                   ))
@@ -49,5 +53,22 @@ class ScanQrInstructions extends StatelessWidget {
         ],
       )
     );
+  }
+
+  Future scan() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      Fluttertoast.showToast(msg: barcode);
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        Fluttertoast.showToast(msg: 'The user did not grant the camera permission');
+      } else {
+        Fluttertoast.showToast(msg: 'Error: $e');
+      }
+    } on FormatException{
+      Fluttertoast.showToast(msg: 'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      Fluttertoast.showToast(msg: 'Error: $e');
+    }
   }
 }
